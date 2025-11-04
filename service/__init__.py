3fef1f349d52fca7d52365fd0fa6c3d29ba2210b
aashiimathur <thebookharbor@gmail.com>
2025-11-04 11:52:36 -0500

    Fix flake8 lint issue and update project files

--- service/__init__.py
+++ service/__init__.py
@@ -1,33 +1,35 @@
 """
 Package: service
-Package for the application models and service routes
-This module creates and configures the Flask app and sets up the logging
-and SQL database
+Package for the application models and service routes.
+This module creates and configures the Flask app and sets up logging
+and the SQL database.
 """
 import sys
 from flask import Flask
-from service import config
-from service.common import log_handlers
 from flask_talisman import Talisman
 from flask_cors import CORS
+from service import config
+from service.common import log_handlers
 
 app = Flask(__name__)
 app.config.from_object(config)
 CORS(app)
 talisman = Talisman(app)
 
-# Import the routes After the Flask app is created
+# Import routes after Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
-from service import routes, models  # noqa: F401 E402
+from service import routes, models  # noqa: F401, E402
 
 # pylint: disable=wrong-import-position
-from service.common import error_handlers, cli_commands  # noqa: F401 E402
+from service.common import error_handlers, cli_commands  # noqa: F401, E402
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(
+    "  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*")
+)
 app.logger.info(70 * "*")
 
 try:
